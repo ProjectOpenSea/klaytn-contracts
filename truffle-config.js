3c29aaf37cccb7e129ea41a621307a7de76e80bb
Junghyun Colin Kim <colin.kim@groundx.xyz>
2021-03-22 13:20:55 +0900

    Update compiler version for 0x

--- truffle-config.js
+++ truffle-config.js
@@ -154,12 +154,13 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.6",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 200
+          runs: 1000000,
+          details: { yul: true, deduplicate: true, cse: true, constantOptimizer: true },
         },
         evmVersion: "constantinople"
       }
